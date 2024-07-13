f143bd3b16914b9b61a536822de2212a7169fe01
sumit <suberquercusumit@gmail.com>
2024-07-13 22:38:10 +0530

    add voter project registerCandidate & isVoterNotRegistered complete

--- VotingProject/contracts/storage.sol
+++ VotingProject/contracts/storage.sol
@@ -1,20 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-
 pragma solidity ^0.8.26;
 
-
-
-
 contract Vote {
-
-   //first entity
+    //first entity
     struct Voter {
         string name;
-        uint age;
-        uint voterId;
+        uint256 age;
+        uint256 voterId;
         Gender gender;
-        uint voteCandidateId; //candidate id to whom the voter has voted
+        uint256 voteCandidateId; //candidate id to whom the voter has voted
         address voterAddress; //EOA of the voter
     }
 
@@ -22,128 +17,130 @@ contract Vote {
     struct Candidate {
         string name;
         string party;
-        uint age;
+        uint256 age;
         Gender gender;
-        uint candidateId;
+        uint256 candidateId;
         address candidateAddress; //condidate EOA
-        uint votes; //number of votes
+        uint256 votes; //number of votes
     }
 
-
-   //third entity
+    //third entity
     address electionCommission;
 
     address public winner;
 
-    uint nextVoterId = 1;
-    uint nextCandidateId = 1;
+    uint256 nextVoterId = 1;
+    uint256 nextCandidateId = 1;
 
     //voting period
-    uint startTime;
-    uint endTime;
+    uint256 startTime;
+    uint256 endTime;
     bool stopVoting;
 
+    mapping(uint256 => Voter) voterDetails;
+    mapping(uint256 => Candidate) candidateDetails;
 
-    mapping(uint => Voter) voterDetails;
-    mapping(uint => Candidate) candidateDetails;
-
-
-    enum VotingStatus {NotStarted, InProgress, Ended}
-    enum Gender {NotSpecified, Male, Female, Other}
-
+    enum VotingStatus {
+        NotStarted,
+        InProgress,
+        Ended
+    }
+    enum Gender {
+        NotSpecified,
+        Male,
+        Female,
+        Other
+    }
 
     constructor() {
         electionCommission = msg.sender; // msg.sender is a global variable (current call hold address)
-        
     }
-
 
     modifier isVotingOver() {
-        require(block.timestamp <=endTime && stopVoting==false,"Voting time is over");
-      _;
-    }
-
-
-    modifier onlyCommissioner() {
-        require(msg.sender==electionCommission,"Not authuorized");
+        require(
+            block.timestamp <= endTime && stopVoting == false,
+            "Voting time is over"
+        );
         _;
     }
 
+    modifier onlyCommissioner() {
+        require(msg.sender == electionCommission, "Not authuorized");
+        _;
+    }
 
     function registerCandidate(
         string calldata _name,
         string calldata _party,
-        uint _age,
+        uint256 _age,
         Gender _gender
     ) external {
-       
+        require(_age >= 18, "You are below 18");
+        require(
+            isCandidateNotRegistered(msg.sender),
+            "You are already registered"
+        );
+        require(nextCandidateId < 3, "Candidate Registration Full");
+        require(
+            msg.sender != electionCommission,
+            "Election Commision not allowed to register"
+        );
+
+        candidateDetails[nextCandidateId] = Candidate({
+            name: _name,
+            party: _party,
+            age: _age,
+            gender: _gender,
+            candidateId: nextCandidateId,
+            candidateAddress: msg.sender,
+            votes: 0
+        });
+        nextCandidateId++;
     }
 
+    function isCandidateNotRegistered(
+        address _person
+    ) internal view returns (bool) {}
 
-    function isCandidateNotRegistered(address _person) internal view returns (bool) {
-           
-    }
+    function getCandidateList() public view returns (Candidate[] memory) {}
 
-
-    function getCandidateList() public view returns (Candidate[] memory) {
-     
-    }
-
-
-    function isVoterNotRegistered(address _person) internal view returns (bool) {
-         
-    }
-
+    function isVoterNotRegistered(
+        address _person
+    ) internal view returns (bool) {}
 
     function registerVoter(
         string calldata _name,
-        uint _age,
+        uint256 _age,
         Gender _gender
     ) external {
+        require(_age >= 18, "You are below 18");
+        require(isVoterNotRegistered(msg.sender), "You are already registered");
 
-
-
-
+        voterDetails[nextVoterId] = Voter({
+            name: _name,
+            age: _age,
+            voterId: nextVoterId,
+            gender: _gender,
+            voteCandidateId: 0,
+            voterAddress: msg.sender
+        });
+        nextVoterId++;
     }
 
+    function getVoterList() public view returns (Voter[] memory) {}
 
-    function getVoterList() public view returns (Voter[] memory) {
-   
-    }
+    function castVote(uint256 _voterId, uint256 _id) external {}
 
+    function setVotingPeriod(
+        uint256 _startTime,
+        uint256 _endTime
+    ) external onlyCommissioner {}
 
-    function castVote(uint _voterId, uint _id) external {
-   
-    }
+    function getVotingStatus() public view returns (VotingStatus) {}
 
+    function announceVotingResult() external onlyCommissioner {}
 
-    function setVotingPeriod(uint _startTime, uint _endTime) external onlyCommissioner() {
-   
-    }
-
-
-    function getVotingStatus() public view returns (VotingStatus) {
-   
-    }
-
-
-    function announceVotingResult() external onlyCommissioner() {
-   
-    }
-
-
-    function emergencyStopVoting() public onlyCommissioner() {
-       stopVoting=true;
+    function emergencyStopVoting() public onlyCommissioner {
+        stopVoting = true;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
